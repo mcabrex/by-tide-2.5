982b3d4ec1598c37c2b0686408cac54df85c2560
mcabrex <mrcabardo@gmail.com>
2022-07-08 22:47:17 -0400

    HEADER: root log

--- assets/header.js
+++ assets/header.js
@@ -71,10 +71,11 @@ class Header extends HTMLElement {
         this.fixed_state = e, this.setAttribute("data-fixed", e)
         if(e){
             this.trigger("fixed")
-            this.css('background-color','transparent')
+            this.root.css('background-color','transparent')
             console.log('header',this)
         } else {
             this.trigger("unfixed")
+            console.log('header',this)
         }
     }
 }
