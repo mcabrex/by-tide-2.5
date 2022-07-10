6e4671e5a76dd6e004ad2cba309e48acf3866c80
mcabrex <mrcabardo@gmail.com>
2022-07-10 03:40:07 -0400

    HEADER: this was kind of a lot

--- assets/header.js
+++ assets/header.js
@@ -27,7 +27,11 @@ class Header extends HTMLElement {
         }], this.load()
     }
     load() {
-        this.element_pairs.forEach(e => this.moveElement(e.parent, e.child)), Shopify.designMode && (this.sectionListeners(), this.inspectListeners()), this.fixed_enabled && (this.header_fill = this.previousElementSibling, this.initFixed(), window.on("theme:XMenu:loaded", () => this.initFixed()))
+        this.element_pairs.forEach(e => this.moveElement(e.parent, e.child)), Shopify.designMode && (this.sectionListeners(), this.inspectListeners()), this.fixed_enabled && (this.header_fill = this.previousElementSibling, this.initFixed(), window.on("theme:XMenu:loaded", () => this.initFixed()));
+        if(this.isHomePage){
+            this.style.backgroundColor = 'transparent'
+            this.style.borderBottom = 'none'
+        }
     }
     moveElement(e, t) {
         e && (e.innerHTML = ""), e && t && e.appendChild(t)
@@ -54,14 +58,18 @@ class Header extends HTMLElement {
     setHeaderFill() {
         //TRACK THE PIXEL THRESHOLD
         if(this.isHomePage){
-            this.header_fill.style.height = 0, this.style.top = this.header_fill.offset().top + "px"
+            this.header_fill.style.height = 0 + "px"
         } else {
             this.header_fill.style.height = this.unfixed_height + "px", this.style.top = this.header_fill.offset().top + "px"
         }
         // this.header_fill.style.height = 0 + "px", this.style.top = this.header_fill.offset().top + "px"
     }
     setThresholdValues() {
-        this.pixel_threshold = this.unfixed_height - this.fixed_height, this.observer_threshold = +(1 - this.pixel_threshold / this.unfixed_height).toFixed(4), 1 < this.observer_threshold && (this.observer_threshold = 1)
+        if(this.isHomePage){
+            this.pixel_threshold = 0, this.observer_threshold = +(1 - this.pixel_threshold / this.unfixed_height).toFixed(4), 1 < this.observer_threshold && (this.observer_threshold = 1)
+        } else {
+            this.pixel_threshold = this.unfixed_height - this.fixed_height, this.observer_threshold = +(1 - this.pixel_threshold / this.unfixed_height).toFixed(4), 1 < this.observer_threshold && (this.observer_threshold = 1)
+        }
     }
     createObserver() {
         this.observer && this.observer.unobserve(this.header_fill), this.observer = new IntersectionObserver(() => this.detectAndFixHeader(), {
@@ -71,17 +79,10 @@ class Header extends HTMLElement {
     detectAndFixHeader() {
         if (!Shopify.inspectMode) {
             let e;
-            e = this.announcement ? this.pixel_threshold + this.announcement.offsetHeight : this.pixel_threshold 
-            
-            if(this.isHomePage){
-                window.pageYOffset >= this.announcement.offsetHeight && !this.fixed_state ? this.fixHeader(!0) : window.pageYOffset < e && this.fixed_state && this.fixHeader(!1)
-            } else {
-                window.pageYOffset >= e && !this.fixed_state ? this.fixHeader(!0) : window.pageYOffset < e && this.fixed_state && this.fixHeader(!1)
-            }
+            e = this.announcement ? this.pixel_threshold + this.announcement.offsetHeight : this.pixel_threshold,             window.pageYOffset >= e && !this.fixed_state ? this.fixHeader(!0) : window.pageYOffset < e && this.fixed_state && this.fixHeader(!1)
             console.log('pixel threshold e',{
                 e,
                 pixelThresh: this.pixel_threshold,
-                anouncedOffset: this.announcement.offsetHeight,
                 pageyoffy: window.pageYOffset,
                 fixed: this.fixed_state,
                 webber: this.isHomePage
