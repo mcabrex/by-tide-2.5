69394f81c64581f8f4073f36a8f6d0359cc9986e
mcabrex <mrcabardo@gmail.com>
2022-07-20 17:12:11 -0400

    HEADER: to e or not to e

--- assets/header.js
+++ assets/header.js
@@ -68,7 +68,7 @@ class Header extends HTMLElement {
     }
     fixHeader(e) {
         this.fixed_state = e, this.setAttribute("data-fixed", e), e ? this.trigger("fixed") : this.trigger("unfixed")
-        if(e){
+        if(!e){
             this.header_fill.style.backgroundColor = 'transparent'
             this.style.backgroundColor = 'transparent'
         }
