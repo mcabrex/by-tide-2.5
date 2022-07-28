a03e9829e169b7bb2b6bd3927c2fc315bfbfda68
mcabrex <mrcabardo@gmail.com>
2022-07-28 01:00:30 -0400

    HEADER: pixel threshold test 1

--- assets/header.js
+++ assets/header.js
@@ -53,7 +53,7 @@ class Header extends HTMLElement {
         this.header_fill.style.height = 0 + "px", this.style.top = this.header_fill.offset().top + "px"
     }
     setThresholdValues() {
-        this.pixel_threshold = this.unfixed_height - this.fixed_height, this.observer_threshold = +(1 - this.pixel_threshold / this.unfixed_height).toFixed(4), 1 < this.observer_threshold && (this.observer_threshold = 1)
+        this.pixel_threshold = 0 + this.fixed_height, this.observer_threshold = +(1 - this.pixel_threshold / this.unfixed_height).toFixed(4), 1 < this.observer_threshold && (this.observer_threshold = 1)
     }
     createObserver() {
         this.observer && this.observer.unobserve(this.header_fill), this.observer = new IntersectionObserver(() => this.detectAndFixHeader(), {
@@ -67,7 +67,7 @@ class Header extends HTMLElement {
             if(window.pageYOffset >= e && !this.fixed_state){
                 this.fixHeader(!0)
             } else {
-                window.pageYOffset < e && this.fixed_state && this.fixHeader(!1)
+                window.pageYOffset < e && this.fixed_state && this.fixHeader(!1) 
                 this.style.backgroundColor = "transparent"
             }
         }
