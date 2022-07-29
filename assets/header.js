73a8758ecddc136d6dfc256afa4be3140b01f280
mcabrex <mrcabardo@gmail.com>
2022-07-29 12:48:23 -0400

    HEADER: greater than e

--- assets/header.js
+++ assets/header.js
@@ -71,7 +71,7 @@ class Header extends HTMLElement {
             let e;
             if(window.location.pathname == "/"){
                 e = this.announcement ? 0 + this.announcement.offsetHeight : 0, window.pageYOffset >= e && !this.fixed_state ? this.fixHeader(!0) : window.pageYOffset < e && this.fixed_state && this.fixHeader(!1)
-                if(window.pageYOffset > e){
+                if(window.pageYOffset >= e){
                     this.style.backgroundColor = "var(--bg-color--header)"
                 } else {
                     this.style.backgroundColor = "transparent"
