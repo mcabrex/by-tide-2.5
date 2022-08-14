82a1a8ae409534a514b7e0db3016b183d0468a12
mcabrex <mrcabardo@gmail.com>
2022-08-13 22:07:36 -0400

    HEADER: ishome

--- assets/header.js
+++ assets/header.js
@@ -70,6 +70,7 @@ class Header extends HTMLElement {
         if (!Shopify.inspectMode) {
             let e;
             if(window.location.pathname == "/"){
+                console.log('ishome')
                 e = this.announcement ? 0 + this.announcement.offsetHeight : 0, window.pageYOffset >= e && !this.fixed_state ? this.fixHeader(!0) : window.pageYOffset < e && this.fixed_state && this.fixHeader(!1)
                 if(window.pageYOffset >= e){
                     this.style.backgroundColor = "var(--bg-color--header)"
